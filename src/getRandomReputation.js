86b5a8d236939b933bcaf0aa4e2a721eb8d49d5a
Raul Glogovetan <raul@glogovetan.com>
2020-11-24 18:38:54 +0200

    Refactor: `getRandomReputation` better randomness

--- src/getRandomReputation.js
+++ src/getRandomReputation.js
@@ -5,7 +5,11 @@ import constants from './constants';
 const { WEI } = constants;
 
 export default (currentAccountIndex, baseReputationMultiplier = 100) => {
-  const lameRandomness = Math.floor(Math.random() * 10);
+  /*
+   * Radom between 1 and 10
+   * min + floor(random * (max - min + 1))
+   */
+  const lameRandomness = 1 + Math.floor(Math.random() * 10);
   const oneWei = new BigNumber(WEI);
   const reputationInWei = oneWei
     .mul(new BigNumber(currentAccountIndex))
